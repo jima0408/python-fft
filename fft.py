6cf7e8dbda9d09ac88cdb8272e56833042e77749
jima0408 <jimajima0408yoppa@gmail.com>
2023-07-12 05:42:41 +0900

    複数ファイルからまとめて最終結果まで出せるように調整

--- fft.py
+++ fft.py
@@ -3,60 +3,86 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sys
+import csv
+import os
+import re
 
+# 引数からファイル名を取得
 args = sys.argv
 wave_path = args[1]
+files = os.listdir(wave_path)
 
-#書き出し用のcsvデータを抽出
-df = pd.read_csv(wave_path, header=None)
+# 結果を保存するcsvファイルを作成
+with open(args[3] + "/result.csv", "w", newline='') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerow(["frequency /Hz", "amplitude /V"])
 
-#csvから設定値を抽出
-wave_setting_data = pd.read_csv(wave_path, header=None, usecols=[1])
-WAVE_SMP_TIME = wave_setting_data.values[1]   #[sec] => 10us
-wave_record_count = wave_setting_data.values[0]
+for file in files:
+    isCsv = re.search('.csv', file)
+    if isCsv:
+        #ファイル名から波長を取得
+        wave_length = re.search('(\d+)', file).group(1)
+        path = args[1] + "/" + file
+        print(file)
 
-#①波形データの読み出しとフーリエ変換後のX軸単位を計算
-wave_df = pd.read_csv(wave_path, header=None, usecols=[4])
-smp_count = len(wave_df.index.values)
-x_unit_fft = 1/WAVE_SMP_TIME/smp_count  #[Hz]
-print(str(x_unit_fft)+"[Hz]")
+        #csvから設定値を抽出
+        wave_setting_data = pd.read_csv(path, header=None, usecols=[1])
+        WAVE_SMP_TIME = wave_setting_data.values[1]   #[sec] => 10us
+        wave_record_count = wave_setting_data.values[0]
 
-#②フーリエ変換の実行
-##フーリエ変換後のX座標単位からグラフプロット用のリストを作成する
-fft_x = [i*x_unit_fft for i in range(smp_count)]
-##FFT処理を実施する
-wave_fft = np.fft.fft(wave_df.iloc[:,0].values)
-##処理後の値は複素数なので絶対値をとる
-wave_fft_abs=abs(wave_fft)
+        # 波形データの読み出しとフーリエ変換後のX軸単位を計算
+        wave_df = pd.read_csv(path, header=None, usecols=[4])
+        smp_count = len(wave_df.index.values)
+        x_unit_fft = 1/WAVE_SMP_TIME/smp_count  #[Hz]
+        print("FFT interval: " + str(x_unit_fft[0]) + " [Hz]")
 
-##振幅の算出
-amplitude = wave_fft_abs / (smp_count/2)
+        # フーリエ変換の実行
+        ##フーリエ変換後のX座標単位からグラフプロット用のリストを作成する
+        fft_x = [i*x_unit_fft for i in range(smp_count)]
+        ##FFT処理を実施する
+        wave_fft = np.fft.fft(wave_df.iloc[:,0].values)
+        ##処理後の値は複素数なので絶対値をとる
+        wave_fft_abs=abs(wave_fft)
 
-#④FFTによる周波数分析結果をグラフにプロット
-##グラフ描画範囲を設定する
-FQ_SEARCH_ST = 0 #[Hz]
-FQ_SEARCH_ED = float(args[2]) #[Hz]
-st_index = np.abs(np.asarray(fft_x) - FQ_SEARCH_ST).argmin()
-ed_index = np.abs(np.asarray(fft_x) - FQ_SEARCH_ED).argmin()
+        ##振幅の算出
+        amplitude = wave_fft_abs / (smp_count/2)
+
+        # FFTによる周波数分析結果をグラフにプロット
+        ##グラフ描画範囲を設定する
+        FQ_SEARCH_ST = 0 #[Hz]
+        FQ_SEARCH_ED = float(args[2]) #[Hz]
+        st_index = np.abs(np.asarray(fft_x) - FQ_SEARCH_ST).argmin()
+        ed_index = np.abs(np.asarray(fft_x) - FQ_SEARCH_ED).argmin()
+
+        # グラフを描画して[波長].jpgで保存
+        plt.figure(figsize=(10,8))    
+        plt.plot(fft_x[st_index:ed_index],amplitude[st_index:ed_index],lw=1)
+        plt.xlabel("frequency [Hz]")
+        plt.ylabel("amplitude [V]")
+        plt.savefig(args[3] + "/" + wave_length + ".jpg")
+        plt.close()
+
+        #wave_lengthに一番近い波長とその時の振幅を取得
+        picked_wave_length = min(fft_x[st_index:ed_index], key=lambda x:abs(float(x)-float(wave_length)))[0]
+        picked_amplitude = amplitude[fft_x.index(picked_wave_length)]
+        print("wave length: " + str(picked_wave_length) + " [Hz]")
+        print("amplitute: " + str(picked_amplitude) + " [V]")
+
+        # 結果をcsvに保存
+        with open(args[3] + "/result.csv", "a", newline='') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow([picked_wave_length, picked_amplitude])
+        
+    
+# 結果をグラフにプロット
+result_frequency = pd.read_csv(args[3] + "/result.csv", header=1, usecols=[0])
+result_amplitude = pd.read_csv(args[3] + "/result.csv", header=1, usecols=[1])
+xy_sorted = sorted(zip(result_frequency.values, result_amplitude.values))
+sorted_result_frequency, sorted_result_amplitude = zip(*xy_sorted)
 
 plt.figure(figsize=(10,8))    
-plt.plot(fft_x[st_index:ed_index],amplitude[st_index:ed_index],lw=1)
+plt.plot(sorted_result_frequency, sorted_result_amplitude, 'o', lw=1)
 plt.xlabel("frequency [Hz]")
 plt.ylabel("amplitude [V]")
-plt.savefig("./" + args[3] + ".jpg")
-plt.show()
-
-#csvファイル用に時間と周波数の行データを生成
-time = []
-freq = []
-for i in range(smp_count):
-    time.append(float(WAVE_SMP_TIME) * i)
-    freq.append(float(x_unit_fft) * i)
-
-#csvファイルの作成
-df.insert(5, "", "")
-df.insert(6, "time /s" , time)
-df.insert(7, "frequency /Hz", freq)
-df.insert(8, "fft_abs", wave_fft_abs)
-df.insert(9, "amplitude /V", amplitude)
-df.to_csv("./" + args[3] + ".csv")
+plt.savefig(args[3] + "/result.jpg")
+plt.close()
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@
 $scp -r python-fft UECアカウント名@sol.edu.cc.uec.ac.jp:./
 ```
 
-pythonを使うために、solにログインして以下のコードを実行してください。
+pythonを使うために、solにログインして以下のコードを実行してください(初回のみ)
 
 ```
 $module load python/3.10.2  
@@ -15,38 +15,39 @@ $python3 -m pip install matplotlib numpy pandas
 ```
 
 ## 使い方
-まず、ローカルのファイルをsolに持っていきます。
+実験結果は`[波長].csv`の形で一つのディレクトリ内にまとめて配置してください。
+まず、ローカルのディレクトリをsolに持っていきます。
 
 `
-$scp [file_name] UECアカウント名@sol.edu.cc.uec.ac.jp:[fft.py_path]
+$scp [dir_name] UECアカウント名@sol.edu.cc.uec.ac.jp:[fft.py_path]
 `
 
 次に、フーリエ変換をしたいファイルを`fft.py`と同じディレクトリに置いて、以下のコマンドを実行してください。
 `
-$python3 fft.py [file_name] [frequency] [output_name]
+$python3 fft.py [dir] [frequency] [output_dir]
 `
 
-`[file_name]`: フーリエ変換したいcsvファイルの名前
+`[dir]`: データの入ったディレクトリ名
 `[frequency]`: グラフに表示する最大周波数
-`[output_name]`: 出力するファイルの名前
+`[output_dir]`: 結果を出力するディレクトリ名
 
-上のコマンドを実行すると、フーリエ変換されたデータ類が格納された`fuga.csv`と、横軸周波数(3000Hzまで)で縦軸が振幅のグラフ画像の`fuga.jpg`が出力されます。
+上のコマンドを実行すると、`[output_dir]`に各波長のフーリエ変換後のグラフと、最終的な結果である`result.jpg`,`result.csv`が出力されます。
 
 最後に、出力されたファイルをローカルに持っていきます。
 
 `
-scp UECアカウント名@sol.edu.cc.uec.ac.jp:[output_file] [local_path] 
+scp UECアカウント名@sol.edu.cc.uec.ac.jp:[output_dir] [local_path] 
 `
 
-例：`hoge.csv`というファイルを3000Hzまでグラフ化して`fuga.csv`という名前で出力したい
+例：`hoge`内のデータを3000Hzまでグラフ化して`fuga`というディレクトリ名で結果を出力したい
 
 ```
 // ローカルからsolにファイル転送
-$scp hoge.csv UECアカウント名@sol.edu.cc.uec.ac.jp:python-fft/
+$scp hoge UECアカウント名@sol.edu.cc.uec.ac.jp:python-fft/
 
 // solにログインして実行
-$python3 fft.py hoge.csv 3000 fuga 
+$python3 fft.py hoge 3000 fuga 
 
 // solをログアウトしてローカルで実行
-$scp UECアカウント名@sol.edu.cc.uec.ac.jp:python-fft/fuga.csv ./
+$scp UECアカウント名@sol.edu.cc.uec.ac.jp:python-fft/fuga ./
 ```
