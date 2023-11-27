174e4d8eb40b3056c214a38676354c914ad7a938
jima0408 <jimajima0408yoppa@gmail.com>
2023-11-27 18:07:46 +0900

    自動化プログラムを対話式に修正

--- automation.py
+++ automation.py
@@ -3,13 +3,14 @@ visa.log_to_screen()
 import time
 
 def get_args():
+    start_freq = int(input("周波数の開始値を入力してください："))
     termination = int(input("周波数の終端値を入力してください："))
     step_width = int(input("周波数増加のステップ幅を入力してください："))
     save_dir = str(input("保存先のディレクトリ名を指定してください（例: data/20211211)："))
-    return termination, step_width, save_dir
+    return start_freq,termination,step_width,save_dir
 
 # 初期値の取得
-termination, step_width, save_dir = get_args()
+start_freq, termination, step_width, save_dir = get_args()
 
 # pyvisaの初期化
 rm = visa.ResourceManager()
@@ -21,34 +22,38 @@ print("接続されているVISAリソースの一覧:")
 for resource in resources:
     print(resource)
 
-# FGとオシロのアドレス指定
 func_gen = rm.open_resource('GPIB0::11::INSTR')
 scope = rm.open_resource('GPIB0::1::INSTR')
 
-# ファンクションジェネレーターの出力と波形の設定
-func_gen.write('VOLTAGE:AMPLITUDE 10.00') # 最大値(10Vpp)
+
+# ファンクションジェネレーターの出力を最大に設定
+func_gen.write('VOLTAGE:AMPLITUDE 10.00')
+# オシロスコープの出力をsin波に指定
 func_gen.write('FUNCTION SIN')
-# オシロの波形保存形式をcsvに指定
+print("ファンクションジェネレーターの設定完了")
+# オシロスコープの波形保存形式をcsvに指定
 scope.write('SAVe:WAVEform:FILEFormat SPREADSHEETCsv')
+print("オシロスコープの設定完了")
 print("セッティング完了")
 
-freq = 10 # 波長の最低値
+freq = start_freq # 開始周波数
 while freq <= termination:
     print(str(freq) + "Hz")
-    func_gen.write('FREQUENCY '+ str(freq))
-    func_gen.write('OUTPut1:STATe ON')
+    func_gen.write('FREQUENCY '+ str(freq)) # ファンクションジェネレータの周波数設定
+    func_gen.write('OUTPut1:STATe ON') # 設定した周波数で出力
     time.sleep(1)
 
     # 保存先(USB固定)
     csvName = '"E:/' + save_dir + '/' + str(freq) + '.csv"'
+
     print(csvName)
 
-    # 波形の保存(CHは適宜設定)
-    scope.write('SAVe:WAVEform CH2, ' + csvName)
+    scope.write('SAVe:WAVEform CH1, ' + csvName) # CHは使っているチャンネルに応じて適宜変更
     time.sleep(3)
 
     freq += step_width
 
+# 出力を止めて接続解除
 func_gen.write('OUTPut1:STATe OFF')
 func_gen.close()
 scope.close()
