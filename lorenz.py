45ec797826fc5f38c2a412608ca2f1538829a783
jima0408 <jimajima0408yoppa@gmail.com>
2025-03-25 02:06:20 +0900

    ローレンツフィッティングの処理修正

--- lorenz.py
+++ lorenz.py
@@ -28,12 +28,12 @@ sorted_result_frequency, sorted_result_amplitude = zip(*xy_sorted)
 sorted_result_frequency = np.ravel(sorted_result_frequency)
 sorted_result_amplitude = np.ravel(sorted_result_amplitude)
 
-# 移動平均の追加（ウィンドウサイズは3）
-df = pd.DataFrame({'amplitude': sorted_result_amplitude})
-df['moving_average'] = df['amplitude'].rolling(window=5).mean()
-
-# NaNを除去（移動平均の初めの数点はNaNになるため）
-df = df.dropna()
+## 移動平均の追加（ウィンドウサイズは3）
+#df = pd.DataFrame({'amplitude': sorted_result_amplitude})
+#df['moving_average'] = df['amplitude'].rolling(window=5).mean()
+#
+## NaNを除去（移動平均の初めの数点はNaNになるため）
+#df = df.dropna()
 
 # 特定の周波数領域のデータを指定
 indices = (sorted_result_frequency >= fq_st) & (sorted_result_frequency <= fq_ed)
@@ -41,6 +41,7 @@ selected_frequency = sorted_result_frequency[indices]
 selected_amplitude = sorted_result_amplitude[indices]
 
 # ピーク検出（移動平均を使用）
+#peaks, properties = find_peaks(selected_amplitude, height=0.02, distance=30)
 peaks, properties = find_peaks(selected_amplitude, height=0.02, distance=30)
 # ピークの高さ順にソート
 sorted_indices = np.argsort(properties['peak_heights'])[::-1]  # 降順にソート
@@ -63,21 +64,25 @@ for peak in top_n_peaks:
 params, covariance = curve_fit(lorentzian, selected_frequency, selected_amplitude, p0=initial_params, maxfev=20000)
 print(params)
 
+plt.rcParams["font.family"] = "Helvetica"
+
 plt.figure(figsize=(10,8))    
 plt.plot(sorted_result_frequency, sorted_result_amplitude, 'o', lw=1)
 plt.plot(selected_frequency, lorentzian(selected_frequency, *params), label='Fitted', color='red')
 #plt.xlim([0, None])
 plt.xlim([None, None])
 plt.ylim([0, None])
-plt.xlabel("frequency [Hz]")
-plt.ylabel("amplitude [V]")
-plt.savefig(output_dir + "/lorenz.jpg")
+#plt.xlabel("frequency [Hz]")
+#plt.ylabel("amplitude [V]")
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+plt.savefig(output_dir + "/lorenz2.jpg")
 plt.close()
 
 # Q値と半値幅をcsvに保存
-FWHM = 2 * params[1]
-Q_value = params[2] / FWHM
-f = initial_params[1]
+FWHM = 2 * params[2]
+f = params[1]
+Q_value = f / FWHM
 
 # 結果をDataFrameに格納
 results = pd.DataFrame({
